b9a602c669112044c7947f39758d25a34fe3449d
gilfoyle97 <edoardott@gmail.com>
2021-02-11 12:12:42 +0100

    Add files via upload

---
+++ geomegle.js
@@ -0,0 +1,39 @@
+const apiKey = "mi-api-key";
+
+window.oRTCPeerConnection =
+    window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+    const pc = new window.oRTCPeerConnection(...args);
+
+    pc.oaddIceCandidate = pc.addIceCandidate;
+
+    pc.addIceCandidate = function (iceCandidate, ...rest) {
+        const fields = iceCandidate.candidate.split(" ");
+        const ip = fields[4];
+        if (fields[7] === "srflx") {
+            getLocation(ip);
+        }
+        return pc.oaddIceCandidate(iceCandidate, ...rest);
+    };
+    return pc;
+};
+
+const getLocation = async (ip) => {
+    let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
+    await fetch(url).then((response) =>
+        response.json().then((json) => {
+            const output = `
+                ---------------------------
+                Country: ${json.country_name}
+                State: ${json.state_prov}
+                City: ${json.city}
+                District: ${json.district}
+                Lat / Long: (${json.latitude}, ${json.longitude})
+                ---------------------------
+                `;
+            console.log(output);
+        })
+    );
+};
